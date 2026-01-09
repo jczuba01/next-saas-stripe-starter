9b26107159d64ccfeadd5b8af4a50680f796ffee
jczuba01 <103487577+jczuba01@users.noreply.github.com>
2026-01-09 19:14:45 +0100

    Remove all the shiet

--- auth.config.ts
+++ auth.config.ts
@@ -1,20 +1,46 @@
+import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
-import Google from "next-auth/providers/google";
-import Resend from "next-auth/providers/resend";
+import Credentials from "next-auth/providers/credentials";
 
-import { env } from "@/env.mjs";
-import { sendVerificationRequest } from "@/lib/email";
+import { prisma } from "@/lib/db";
 
 export default {
   providers: [
-    Google({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-    Resend({
-      apiKey: env.RESEND_API_KEY,
-      from: env.EMAIL_FROM,
-      // sendVerificationRequest,
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.password,
+        );
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        };
+      },
     }),
   ],
 } satisfies NextAuthConfig;
